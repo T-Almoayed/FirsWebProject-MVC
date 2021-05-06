bec210b7c38223f28aeff0664a59e6091f1f1456
T-Almoayed <Almoayed24@gmail.com>
2021-05-06 12:35:02 +0200

    Final task

---
+++ FirsWebProject/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace FirsWebProject
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ FirsWebProject/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using FirsWebProject.Models;
+using System.Text;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace FirsWebProject.Controllers
+{
+    public class HomeController : Controller
+    {
+        FacultyEntities db = new FacultyEntities();
+
+        public ActionResult Index()
+        {
+            List<Department> list = db.Departments.ToList();
+            return View(list);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
